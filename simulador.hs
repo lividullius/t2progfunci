5c05067d9677212eaa13b34751f073836097e57f
livianoer <livia.dullius@edu.pucrs.br>
2025-06-03 10:57:12 -0300

    Update simulador.hs

--- simulador.hs
+++ simulador.hs
@@ -1,1 +1,13 @@
+//pra criar o simulador tem que definir os tipos e estado da máquina
 
+module Simulador where
+//tipo para representar a memória: lista de pares (endereço, valor)
+type Memory = [(Int, Int)]
+
+//estado completo do computador
+data State = State {
+    memory :: Memory,  -- Memória do computador
+    acc    :: Int,     -- Registrador acumulador
+    eqz    :: Bool,    -- Flag que indica se o ACC é zero
+    ci     :: Int      -- Contador de instruções
+} deriving Show
